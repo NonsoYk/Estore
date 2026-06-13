df9c25b5c6d828ae3df8a030a3bd2c4241d9fde3
NonsoYK <ykingzion369@gmail.com>
2026-06-12 19:31:11 -0700

    gluestack setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config";
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider config={config}>
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
