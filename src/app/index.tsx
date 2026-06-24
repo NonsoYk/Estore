3a44760cf4e3667b6a4524baee5429e5c1876c8f
NonsoYK <ykingzion369@gmail.com>
2026-06-24 16:37:10 -0700

    Gluestack setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import ProductListItem from "@/components/ProductListItem";
-import { Button, ButtonText } from "@/components/ui/button"; 
 import products from "../assets/products.json"
 
 
@@ -10,31 +9,11 @@ export default function Homescreen() {
   
     <FlatList 
     data={products} 
+    numColumns={3}
+    contentContainerClassName="gap-2"
+    columnWrapperClassName="gap-2"
     renderItem={({item}) => <ProductListItem product={item} />}
-    />
-      
-  
- )
- 
- 
-  // return (
-  //<FlatList 
-    //    data={products}
-      //  renderItem={({item}) => <ProductListItem product={item}/>}
-  ///>
-  //);
+    /> 
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center", 
-  },
-  header: {
-    fontSize: 30,
-  },
-  button: {
-    backgroundColor: "blue-600"
-  }
-});
